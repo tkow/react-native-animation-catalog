da69a2a59a79184249309284c66dc87322187e78
tkow <t.kusama0613@outlook.jp>
2019-08-10 00:07:22 +0900

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
+  plugins: ["react", "@typescript-eslint", "prettier"],
   extends: '@react-native-community',
+  parser: "@typescript-eslint/parser",
+  env: {
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
+  parserOptions: {
+    sourceType: "module",
+    project: "./tsconfig.json"
+  },
 };

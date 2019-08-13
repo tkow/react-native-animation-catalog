0e23e08f4ffc65e5141585798e0101c8851ea058
tkow <t.kusama0613@outlook.jp>
2019-08-13 18:10:29 +0900

    continue up straight

--- src/components/BottomBannar/index.tsx
+++ src/components/BottomBannar/index.tsx
@@ -1,15 +1,75 @@
-import React from 'react';
-import { buttonContainerProps,styles } from './style';
-import { GestureResponderEvent,View,TouchableOpacity,Text } from 'react-native';
+import React, {useState,useEffect} from 'react';
+import { Animated, GestureResponderEvent, Text, TouchableOpacity } from 'react-native';
+import { buttonContainerProps, styles } from './style';
 
 interface BottomBannerFormProps {
   visible?: boolean;
   onPress: (event: GestureResponderEvent) => void;
 }
 
+
+const annimatedPopUp = (positionY: Animated.Value,visible?: boolean) => {
+  Animated.decay(positionY, {
+    velocity: visible ? 0.5 : -0.5,
+  }).start();
+}
+// const animConfig =
+//       this.current && this.props.reverseConfig
+//         ? this.props.reverseConfig
+//         : this.props.config;
+//     this.current = this.current ? 0 : 1;
+//     const config: Object = {
+//       ...animConfig,
+//       toValue: this.current,
+//     };
+
+//     Animated[this.props.type](this.state.native, {
+//       ...config,
+//       useNativeDriver: true,
+//     }).start();
+//     Animated[this.props.type](this.state.js, {
+//       ...config,
+//       useNativeDriver: false,
+//     }).start();
+
+    // type="decay"
+    // config={{velocity: 0.5}}
+    // reverseConfig={{velocity: -0.5}}>
+    // {anim => (
+    //   <Animated.View
+    //     style={[
+    //       styles.block,
+    //       {
+    //         transform: [
+    //           {
+    //             translateX: anim,
+    //           },
+    //         ],
+    //       },
+    //     ]}
+    //   />
+    // )}
+
+
 export const BottomBanner = (props: BottomBannerFormProps) => {
-  return props.visible ? (
-    <View style={styles.bottomBunnerLayout}>
+  const [positionY,setPositionY] = useState(new Animated.Value(0))
+
+  useEffect(()=> {
+    annimatedPopUp(positionY,props.visible)
+  },[props.visible])
+  return (
+    <Animated.View
+      style={[
+        styles.bottomBunnerLayout,
+        {
+          transform: [
+            {
+              translateY: positionY
+            }
+          ]
+        }
+      ]}
+    >
       <TouchableOpacity style={styles.buttonContainer} onPress={props.onPress} {...buttonContainerProps}>
         <Text style={styles.buttonText}>
           {
@@ -18,8 +78,9 @@ export const BottomBanner = (props: BottomBannerFormProps) => {
           }
         </Text>
       </TouchableOpacity>
-    </View>
-  ) : null;
+    </Animated.View>
+  )
+  // ) : null;
 };
 
 BottomBanner.displayName = 'BottomBanner';
--- src/screens/Banner/index.tsx
+++ src/screens/Banner/index.tsx
@@ -1,26 +1,26 @@
 import React, {Fragment, useState} from "react";
 import { BottomBanner } from "../../components/BottomBannar";
 import Sample from "../../components/sample";
-import { Alert,ScrollView, View, Text } from "react-native";
+import { Alert,ScrollView, View, Text, Animated,Easing } from "react-native";
 
 interface BannerAnimationProps {
 
 }
 
+
 export default (props: BannerAnimationProps) => {
   const [bottomBanner, setBottomBanner] = useState({
-    visible: false
+    visible: false,
   });
+
   return (
     <Fragment>
       <ScrollView
         onScrollBeginDrag={e => {
-          setBottomBanner({ visible: true });
+          setBottomBanner({...bottomBanner, visible: true });
         }}
         onScrollEndDrag={e => {
-          setTimeout(() => {
-            setBottomBanner({ visible: false });
-          }, 2000);
+          setBottomBanner({...bottomBanner, visible: false });
         }}
       >
         <View style={{height:1000, width:'100%', justifyContent: "center",alignItems: "center"}}>

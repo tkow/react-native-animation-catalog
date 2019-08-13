7e69a25b743ab51a864346f46aca3bfc759e9f37
tkow <t.kusama0613@outlook.jp>
2019-08-13 16:01:29 +0900

    invisible set button

--- src/screens/Banner/index.tsx
+++ src/screens/Banner/index.tsx
@@ -1,18 +1,36 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
 import { BottomBanner } from "../../components/BottomBannar";
 import Sample from "../../components/sample";
-import { Alert } from "react-native";
+import { Alert,ScrollView, View, Text } from "react-native";
 
 interface BannerAnimationProps {
 
 }
 
 export default (props: BannerAnimationProps) => {
+  const [bottomBanner, setBottomBanner] = useState({
+    visible: false
+  });
   return (
     <Fragment>
-      <Sample/>
+      <ScrollView
+        onScrollBeginDrag={e => {
+          setBottomBanner({ visible: true });
+        }}
+        onScrollEndDrag={e => {
+          setTimeout(() => {
+            setBottomBanner({ visible: false });
+          }, 2000);
+        }}
+      >
+        <View style={{height:1000, width:'100%', justifyContent: "center",alignItems: "center"}}>
+          <Text>
+            test
+          </Text>
+        </View>
+      </ScrollView>
       <BottomBanner
-        visible={true}
+        visible={bottomBanner.visible}
         onPress={()=> {
           Alert.alert('test','test')
         }}
